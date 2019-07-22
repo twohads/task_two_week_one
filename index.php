7ef11e0e4533d663c2ebad178a65c1800195a492
Pavel <pavel85_07@bk.ru>
2019-07-22 12:56:03 +0300

    Второе задание, проработка один

---
+++ index.php
@@ -0,0 +1,20 @@
+<?php
+declare(strict_types=1);
+require_once 'src/functions.php';
+
+/* $hotKey = ['Ctrl + ALT + L','Ctrl + Shift + F', 'Ctrl + Shift + up', 'ALT + Shift + down']; - Как передать в task1
+ссылку на массив в виде переменной? */
+
+//task1(['Ctrl + ALT + L','Ctrl + Shift + F', 'Ctrl + Shift + up', 'ALT + Shift + down'], false);
+
+
+//echo task2(3, 1,2,3);
+
+//echo task3(1, 5);
+
+//echo task4();
+
+//echo task5('Карл у Клары украл Кораллы', 1).PHP_EOL;
+//echo task5('Две бутылки лимонада');
+
+//task6("Hello again!");
